e059c01fcda960a31f41b69d34ebb110e7a5dd6e
lobinhodev <contatolobinhodev@gmail.com>
2022-08-09 20:34:23 -0300

    ✨ feat:implementando um formatador de hora em js

---
+++ frontend/js/formatar_hora/formatar_hora.js
@@ -0,0 +1,20 @@
+const currentTime = () => {
+    let h1 = document.querySelector('.h1')
+
+    let date = new Date()
+
+    let hh = date.getHours()
+    let mm = date.getMinutes()
+    let ss = date.getSeconds()
+
+    hh = hh < 10 ? `0${hh}` : hh
+    mm = mm < 10 ? `0${mm}` : mm
+    ss = ss < 10 ? `0${ss}` : ss
+
+    let time = `${hh}:${mm}:${ss}`
+
+    h1.innerText = time
+}
+
+currentTime()
+setInterval(currentTime, 1000)
