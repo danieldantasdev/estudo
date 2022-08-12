1028ab4a1e2c3b537120fc6fae46675d1c93c6ca
lobinhodev <contatolobinhodev@gmail.com>
2022-08-12 18:42:50 -0300

    ✨ feat:implementando api de países

--- backend/javascript/api_countries/js/script.js
+++ backend/javascript/api_countries/js/script.js
@@ -1,5 +1,7 @@
-const FETCH_COUNTY = async (name) => {
-    await fetch(`https://restcountries.com/v3.1/name/${name}`)
+const countryName = document.getElementById('country_name')
+
+const fetchPokemon = async (name) => {
+    await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
         .then((response) => {
             if (response.status === 200) {
                 return response.json()
@@ -7,37 +9,37 @@ const FETCH_COUNTY = async (name) => {
         })
         .then((data) => {
             if (data !== undefined) {
-                return data
+                countryName.innerHTML = data[0].name.common
             } else {
-                return 'Country not found'
+                countryName.innerHTML = 'Pokemon not found'
             }
+            console.log(data)
         })
         .catch((error) => {
             console.log(error)
         })
 }
 
-const countryName = document.querySelector('.country__name')
-const countryCapital = document.querySelector('.country__capital')
-const countryPopulation = document.querySelector('.country__population')
-const countryFlag = document.querySelector('.country__flag')
+const renderPokemon = async (name) => {
+    let data = await fetchPokemon(name)
 
-const RENDER_COUNTY = async (name) => {
-    let data = await FETCH_COUNTY(name)
-
-    countryName.innerHTML = 'loading...'
-    countryCapital.innerHTML = 'loading...'
-    countryPopulation.innerHTML = 'loading...'
-    countryFlag.src = 'https://i.imgur.com/dJa4Z9q.png'
-    if (data !== 'Country not found') {
-        countryName.innerHTML = data[0].cca2
+    if (data !== undefined) {
+        countryName.innerHTML = data[0].name.common
     } else {
-        countryName.innerHTML = data
-        countryCapital.innerHTML = data
-        countryPopulation.innerHTML = data
-        countryFlag.src = 'https://i.imgur.com/dJa4Z9q.png'
+        countryName.innerHTML = 'Pokemon not found'
     }
 }
 
-FETCH_COUNTY('Poland')
-RENDER_COUNTY('Poland')
+// fetchPokemon(
+//     setInterval(() => {
+//         setTimeout(() => {
+//             countryName.innerHTML = 'loading...'
+//         }, 1000)
+//         fetchPokemon(Math.floor(Math.random() * 802) + 1)
+//     }, 2000),
+// )
+
+// fetchPokemon(pokemon)
+
+renderPokemon('Brazil')
+// fetchPokemon('Brazil')
