882f40c68fadbfa20ea96afe1961fcfad6097559
lobinhodev <contatolobinhodev@gmail.com>
2022-08-18 21:16:13 -0300

    ✨ feat:implementando o id dinâmico pela rota

--- frontend/angular/product_list/product_list/src/app/components/product-detail/product-detail.component.ts
+++ frontend/angular/product_list/product_list/src/app/components/product-detail/product-detail.component.ts
@@ -12,13 +12,12 @@ export class ProductDetailComponent implements OnInit {
 
   product: Product | undefined
 
-
   ngOnInit(): void {
       // First get the product id from the current route.
       const routeParams = this.route.snapshot.paramMap
       const productIdFromRoute = Number(routeParams.get('productId'))
 
       // Find the product that correspond with the id provided in route.
-      this.product = products.find((product) => product.id === productIdFromRoute)
+    this.product = products.find((product) => product.id === productIdFromRoute)
   }
 }
