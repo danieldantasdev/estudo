5aa07717b1d0768b845204f50e9b703361092553
lobinhodev <contatolobinhodev@gmail.com>
2022-10-18 21:15:48 -0300

    🎉 init:commit inicial

---
+++ mobile/ionic/capacitor/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
