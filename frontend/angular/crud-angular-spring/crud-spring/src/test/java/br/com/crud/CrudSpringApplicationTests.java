aae4d27d007f784e66f168c8bae1e6b84439ef52
lobinhodev <contatolobinhodev@gmail.com>
2022-11-06 12:28:43 -0300

    🎉 init:commit inicial do projeto em spring

---
+++ frontend/angular/crud-angular-spring/crud-spring/src/test/java/br/com/crud/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
